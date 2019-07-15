1865836883a969a52b868732c8a2d019e2c429b2
Gill Abada <gill@callifi.com>
2019-07-15 15:37:51 -0400

    add board and grid.

--- gill-gameoflife/src/App.js
+++ gill-gameoflife/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
+import Game from './Game';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <>
+          <h1>Conway's Game of Life</h1>
+          <Game />
+        </>
       </header>
     </div>
   );
