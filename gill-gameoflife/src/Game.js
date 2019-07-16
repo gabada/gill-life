9551ed9641d542330d317a3c22cea55d2762e827
Gill Abada <gill@callifi.com>
2019-07-16 16:48:26 -0400

    complete mvp and some stretch.

--- gill-gameoflife/src/Game.js
+++ gill-gameoflife/src/Game.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Game.css';
 
 const CELL_SIZE = 20;
-const WIDTH = 800;
-const HEIGHT = 600;
+const WIDTH = 1000;
+const HEIGHT = 800;
 
 class Cell extends React.Component {
   render() {
@@ -29,8 +29,7 @@ class Game extends React.Component {
     this.cols = WIDTH / CELL_SIZE;
     this.board = this.makeEmptyBoard();
   }
-  state = { cells: [] };
-  // Create an empty board
+  state = { cells: [], interval: 100, isRunning: false, count: 0 };
   makeEmptyBoard() {
     let board = [];
     for (let y = 0; y < this.rows; y++) {
@@ -50,7 +49,7 @@ class Game extends React.Component {
       y: rect.top + window.pageYOffset - doc.clientTop
     };
   }
-  // Create cells from this.board
+
   makeCells() {
     let cells = [];
     for (let y = 0; y < this.rows; y++) {
@@ -79,25 +78,158 @@ class Game extends React.Component {
     this.setState({ cells: this.makeCells() });
   };
 
+  runGame = () => {
+    this.setState({ isRunning: true });
+    this.runIteration();
+  };
+
+  stopGame = () => {
+    this.setState({ isRunning: false });
+    if (this.timeoutHandler) {
+      window.clearTimeout(this.timeoutHandler);
+      this.timeoutHandler = null;
+    }
+  };
+
+  calculateNeighbors(board, x, y) {
+    let neighbors = 0;
+    const dirs = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1]
+    ];
+    for (let i = 0; i < dirs.length; i++) {
+      const dir = dirs[i];
+      let y1 = y + dir[0];
+      let x1 = x + dir[1];
+
+      if (
+        x1 >= 0 &&
+        x1 < this.cols &&
+        y1 >= 0 &&
+        y1 < this.rows &&
+        board[y1][x1]
+      ) {
+        neighbors++;
+      }
+    }
+
+    return neighbors;
+  }
+
+  handleIntervalChange = event => {
+    this.setState({ interval: event.target.value });
+  };
+
+  handleClear = () => {
+    this.board = this.makeEmptyBoard();
+    this.setState({ cells: this.makeCells(), count: 0 });
+  };
+
+  handleRandom = () => {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        this.board[y][x] = Math.random() >= 0.5;
+      }
+    }
+
+    this.setState({ cells: this.makeCells(), count: 0 });
+  };
+
+  runIteration() {
+    let count = this.state.count;
+    ++count;
+    this.setState({ count: count });
+    let newBoard = this.makeEmptyBoard();
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let neighbors = this.calculateNeighbors(this.board, x, y);
+        if (this.board[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newBoard[y][x] = true;
+          } else {
+            newBoard[y][x] = false;
+          }
+        } else {
+          if (!this.board[y][x] && neighbors === 3) {
+            newBoard[y][x] = true;
+          }
+        }
+      }
+    }
+
+    this.board = newBoard;
+    this.setState({ cells: this.makeCells() });
+
+    this.timeoutHandler = window.setTimeout(() => {
+      this.runIteration();
+    }, this.state.interval);
+  }
+
   render() {
-    const { cells } = this.state;
+    const { cells, isRunning, count } = this.state;
     return (
       <div>
-        <div
-          className='Board'
-          style={{
-            width: WIDTH,
-            height: HEIGHT,
-            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
-          }}
-          onClick={this.handleClick}
-          ref={n => {
-            this.boardRef = n;
-          }}
-        >
-          {cells.map(cell => (
-            <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`} />
-          ))}
+        {isRunning ? (
+          <div
+            className='Board'
+            style={{
+              width: WIDTH,
+              height: HEIGHT,
+              backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
+            }}
+          >
+            {cells.map(cell => (
+              <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`} />
+            ))}
+          </div>
+        ) : (
+          <div
+            className='Board'
+            style={{
+              width: WIDTH,
+              height: HEIGHT,
+              backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
+            }}
+            onClick={this.handleClick}
+            ref={n => {
+              this.boardRef = n;
+            }}
+          >
+            {cells.map(cell => (
+              <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`} />
+            ))}
+          </div>
+        )}
+        <div className='controls'>
+          <span className='directions'>Update every</span>{' '}
+          <input
+            value={this.state.interval}
+            onChange={this.handleIntervalChange}
+          />{' '}
+          <span className='directions'>msec</span>
+          {isRunning ? (
+            <button className='button' onClick={this.stopGame}>
+              Stop
+            </button>
+          ) : (
+            <button className='button' onClick={this.runGame}>
+              Run
+            </button>
+          )}
+          <button className='button' onClick={this.handleRandom}>
+            Random
+          </button>
+          <button className='button' onClick={this.handleClear}>
+            Clear
+          </button>
+          <p>Generation: {count}</p>
         </div>
       </div>
     );
